64e31c9ceaa6ebba63fe738f4e2b790a5443d2c5
jiyoon0114 <greenpkh02@naver.com>
2026-08-07 18:19:24 +0900

    first commit

---
+++ src/main/java/com/tourswitch/TourswitchApplication.java
@@ -0,0 +1,13 @@
+package com.tourswitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourswitchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourswitchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tourswitch/TourswitchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourswitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourswitchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
